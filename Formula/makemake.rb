40d604fd092cd7559ac07799b074e032f550867f
Drew Malin <drewmalin@gmail.com>
2023-06-17 17:15:21 -0700

    specify python

--- Formula/makemake.rb
+++ Formula/makemake.rb
@@ -6,8 +6,10 @@ class Makemake < Formula
   url "https://github.com/drewmalin/makemake/releases/download/0.0.1/makemake-0.0.1.tar.gz"
   sha256 "4f6c96b9f29fc2b2359dcb6bd3fe51bb0c1ed766a2cdff523bff9c01694c269a"
 
+  depends_on "python@3.x"
+
   def install
-    virtualenv_install_with_resources(:using => "python@3")
+    virtualenv_install_with_resources
   end
 
   test do
